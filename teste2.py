eb42c4a62471f2b65214b71971b883c8762cd116
Bruno Ferreira <brunoeugenio.olinto@gmail.com>
2024-09-30 14:12:46 -0300

    .

--- teste2.py
+++ teste2.py
@@ -1,24 +1,17 @@
-def fibonacci(n):
-
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-    
 def pertence_fibonacci(num):
-
-    fib = 0
-    while fib <= num:
-        if fib == num:
+    a = 0
+    b = 1
+    while b <= num:
+        if b == num:
             return True
-        fib = fibonacci(fib + 1)
-        return False
-    
-    numero = int(input("Digite um número: "))
+        a, b = b, a + b
+    return False
 
-    if pertence_fibonacci(numero):
-        print(f'O número {numero} pertence à sequência de Fibonnacci.')
-    else:
-        print(f'O número {numero} não pertence à sequência de Fibonacci.')
+
+numero = int(input("Digite um número: "))
+
+
+if pertence_fibonacci(numero):
+  print(f"O número {numero} pertence à sequência de Fibonacci.")
+else:
+  print(f"O número {numero} não pertence à sequência de Fibonacci.")
